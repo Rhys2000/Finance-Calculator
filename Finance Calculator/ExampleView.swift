c98c884bc9c450bd05fdc291367245c7644d25ab
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-10-08 16:22:39 -0700

    Added TransactionView and created a new group to hold all objects associated with TRansactionView

---
+++ Finance Calculator/ExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleView.swift
+//  Finance Calculator
+//
+//  Created by Rhys Julian-Jones on 10/8/24.
+//
+
+import SwiftUI
+
+struct ExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleView()
+}
---
+++ Finance Calculator/Transaction/TransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionView.swift
+//  Finance Calculator
+//
+//  Created by Rhys Julian-Jones on 10/8/24.
+//
+
+import SwiftUI
+
+struct TransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionView()
+}
