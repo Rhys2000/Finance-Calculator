0fc9a73e411e1a0a17e843d71967e6e835cbd90f
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-10-08 16:40:25 -0700

    Added a NavigationStack to TransactionView to give the page a title and add toolbar buttons near the top of the device

--- Finance Calculator/Transaction/TransactionView.swift
+++ Finance Calculator/Transaction/TransactionView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct TransactionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("Hello World")
+                .navigationTitle("Transactions")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        EditButton()
+                    }
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {
+                            print("Button Pressed")
+                        }, label: {
+                            Image(systemName: "plus")
+                        })
+                    }
+                }
+        }
     }
 }
 
